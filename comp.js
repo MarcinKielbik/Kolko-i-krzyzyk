ac4d50c72c9bd288314fe8d786f15923fd2d40e6
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2023-03-28 13:34:20 +0200

    Adding playing with computer algorithm

--- comp.js
+++ comp.js
@@ -17,24 +17,27 @@ let options = ['','','','','','','','',''];
 let currentPlayer ='X';
 let running = false;
 
+
 initialize();
 
-
 function initialize() {
-    cells.forEach(cell => cell.addEventListener('click', cellClicked));
-    restartBtn.addEventListener('click', restartGame);
+    cells.forEach((cell) => cell.addEventListener("click", cellClicked));
+    restartBtn.addEventListener("click", restartGame);
     statusText.textContent = `Kolej dla ${currentPlayer}`;
     running = true;
-}
-
-function cellClicked() {
-    const cellIndex = this.getAttribute('cellIndex');
+  }
   
-    if (options[cellIndex] !== '' || !running) {
+function cellClicked() {
+    const cellIndex = this.getAttribute("cellIndex");
+  
+    if (options[cellIndex] !== "" || !running) {
       return;
     }
   
     updateCell(this, cellIndex);
+    setTimeout(() => {
+        computerTurn();
+    }, 5000);
     checkWinner();
   }
 
@@ -44,11 +47,13 @@ function updateCell(cell, index) {
 }
 
 function changePlayer() {
-    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    running = true;
+    if (currentPlayer === "O") {
+      computerTurn();
+    }
     statusText.textContent = `Kolej dla ${currentPlayer}`;
-}
-
-
+}  
 
 function checkWinner() {
     let roundWon = false;
@@ -59,7 +64,7 @@ function checkWinner() {
         const cellB = options[condition[1]];
         const cellC = options[condition[2]];
 
-        if(cellA == '' || cellB == '' || cellC == ''){
+        if(cellA == "" || cellB == "" || cellC == ""){
             continue;
         }
         if(cellA == cellB && cellB == cellC){
@@ -81,9 +86,28 @@ function checkWinner() {
 }
 
 function restartGame() {
-    currentPlayer = 'X';
-    options = ['', '', '', '', '', '', '', '', ''];
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
     statusText.textContent = `Kolej dla ${currentPlayer}`;
-    cells.forEach(cell => cell.textContent = '');
+    cells.forEach(cell => cell.textContent = "");
     running = true;
 }
+
+function computerTurn() {
+    if (currentPlayer === "O") {
+      let availableCells = [];
+      for (let i = 0; i < options.length; i++) {
+        if (options[i] === "") {
+          availableCells.push(i);
+        }
+      }
+  
+      const randomIndex = Math.floor(Math.random() * availableCells.length);
+      const cellIndex = availableCells[randomIndex];
+      const cell = cells[cellIndex];
+  
+      updateCell(cell, cellIndex);
+      checkWinner();
+    }
+  }
+  
